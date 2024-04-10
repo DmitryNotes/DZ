6116d7a7ac9b79c97453712bf03761298b54b4fc
Dmitry Kostenko <dima-kostenko-00@mail.ru>
2024-04-10 11:52:05 +0300

    УТВ

--- DZ1/obj/Debug/net8.0/DZ1.AssemblyInfo.cs
+++ DZ1/obj/Debug/net8.0/DZ1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DZ1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5cb2c1e6bb9d01158eabf6aa99870894d54aea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DZ1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DZ1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DZ3/obj/Debug/net8.0/DZ3.AssemblyInfo.cs
+++ DZ3/obj/Debug/net8.0/DZ3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DZ3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9239654e144c57fd08d0601f7930b560b61a6486")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b5cb2c1e6bb9d01158eabf6aa99870894d54aea3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DZ3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DZ3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
